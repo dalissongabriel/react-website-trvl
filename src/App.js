46506cb8614bc0deb7e5ad5f231b19ae9f478b0d
dalissongabriel <alissongabrieldino@gmail.com>
2021-03-31 16:07:11 -0300

    feat: add navbar in the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Navbar from './components/Navbar'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 const App = () => {
-  return null
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
+    </>
+  )
 }
 
 export default App;
